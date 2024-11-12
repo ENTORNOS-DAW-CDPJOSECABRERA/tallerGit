03f3aae1a940a75ec30d17d9dc77a640a6bd06d7
Jesús Alcocer Fernández <jesusalcocerfdez@cdpjosecabrera.es>
2024-11-12 12:17:15 +0100

    Iniciamos estudio Arrays

---
+++ src/arrays/EstudioArrays.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class EstudioArrays {
+    public EstudioArrays() {
+        System.out.println("Iniciamos estudio Arrays");
+    }
+
+}
--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,7 +1,10 @@
 package vista;
 
+import arrays.EstudioArrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciamos taller Git");
+        EstudioArrays estudio = new EstudioArrays();
     }
 }
