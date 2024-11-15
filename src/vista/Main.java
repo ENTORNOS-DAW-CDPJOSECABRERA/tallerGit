81e2337cc78fdbc7732dd214afc44af5288ac608
Jesús Alcocer Fernández <jesusalcocerfdez@cdpjosecabrera.es>
2024-11-15 14:06:50 +0100

    iniciamos las ramas

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciamos taller Git");
         EstudioArrays estudio = new EstudioArrays();
+        System.out.println("Finalizamos taller Git");
     }
 }
